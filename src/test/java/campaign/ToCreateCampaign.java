e3ba307c0bb831eb0010e2fd407c65b4fca0c820
Aishwarya Soundarya <Aishwarya Soundarya@Aishu0395>
2024-10-18 15:07:45 +0530

    GOOD

--- src/test/java/campaign/ToCreateCampaign.java
+++ src/test/java/campaign/ToCreateCampaign.java
@@ -3,11 +3,12 @@ package campaign;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class ToCreateCampaign1Test {
+public class ToCreateCampaign {
 	@Test
 	public void createCampaign()
 	{
 		Reporter.log("Campaign creted successfully",true);
+		Reporter.log("done",true);
 	}
 	
 
